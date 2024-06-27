44af5c7cfadb26f09d0dffbb8c5055e62dd70cfd
Manicharan <manicharan150@gmail.com>
2024-06-27 20:01:07 +0530

    Used assignments from week one as modules to understand GET method in http

--- Week-02/index.js
+++ Week-02/index.js
@@ -1,3 +1,5 @@
+const anagram = require("../Week-01/01-js/easy/anagram");
+const palindrome = require("../Week-01/01-js/medium/palindrome");
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -17,7 +19,30 @@ function handleFirstRequest(req, res) {
   res.send(answer);
 }
 
+function handleSecondRequest(req, res) {
+  var str1 = req.query.str1;
+  var str2 = req.query.str2;
+  var answer = anagram(str1, str2);
+  if (answer) {
+    res.send("The Given Strings are Anagrams");
+  } else {
+    res.send("The Given Strings are not Anagrams");
+  }
+}
+
+function handleThirdRequest(req, res) {
+  var str1 = req.query.str1;
+  var answer = palindrome(str1);
+  if (answer) {
+    res.send("The Given String is a Palindrome");
+  } else {
+    res.send("The Given String is not a Palindrome");
+  }
+}
+
 app.get("/sum", handleFirstRequest);
+app.get("/isanagram", handleSecondRequest);
+app.get("/ispalindrome", handleThirdRequest);
 
 function started() {
   console.log(`Server started at http://localhost:${port}`);
